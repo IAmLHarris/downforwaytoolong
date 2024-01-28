3033e26c546acff14934f56e676a042b90fc41ed
IAmLHarris <har21091@byui.edu>
2024-01-27 20:06:31 -0600

    sisyphus

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
+const baseController = require("./controllers/baseController");
 
 /* ***********************
  * View Engine and Templates
@@ -26,6 +27,11 @@ app.set("layout", "./layouts/layout"); // not at views root
  *************************/
 app.use(static);
 //index route
+app.get("/", baseController.buildHome);
+
+// Inventory routes
+app.use("/inv", inventoryRoute);
+
 app.get("/", function (req, res) {
   res.render("index", { title: "Home" });
 });
