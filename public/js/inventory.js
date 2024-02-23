516abed3f76cd962875b552587e01e3e10090345
IAmLHarris <har21091@byui.edu>
2024-02-22 18:20:50 -0600

    manipulating table not working

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,11 +1,12 @@
 "use strict";
 
 // Get a list of items in inventory based on the classification_id
-let cars = document.querySelector("#cars");
-cars.addEventListener("change", function () {
-  let classification_id = cars.value;
+let classificationList = document.querySelector("#classification_id");
+classificationList.addEventListener("change", function () {
+  let classification_id = classificationList.value;
   console.log(`classification_id is: ${classification_id}`);
   let classIdURL = "/inv/getInventory/" + classification_id;
+  console.log(classIdURL);
   fetch(classIdURL)
     .then(function (response) {
       if (response.ok) {
